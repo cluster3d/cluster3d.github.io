12a0e38f83f6f26f600e9a4ec0052f114706675c
Matt-Kou <gtigerrr@gmail.com>
2021-11-23 14:23:38 -0500

    remove HPC job requests

--- evaluation metric/Internal validation indexes/install_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir temp
-cd temp
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-conda env create -f ../environment.yml
-
